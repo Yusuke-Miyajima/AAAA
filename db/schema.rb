25633da218e467aab392762bf70db2a874485733
Yusuke-Miyajima <yuten7.09.01@gmail.com>
2019-08-27 11:34:09 +0000

    InitialCommitMessage

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 2019_08_25_122900) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,10 +31,10 @@ ActiveRecord::Schema.define(version: 2019_08_25_122900) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "profile_image_id"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_image_id"
     t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
